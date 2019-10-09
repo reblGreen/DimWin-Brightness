8b05a02f09a03089947158e57bad7aea2ac41b58
John Earnshaw <john@reblgreen.com>
2019-10-09 02:02:07 +0100

    Added installer progect

--- DimWin/Properties/AssemblyInfo.cs
+++ DimWin/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("dimwin Brightness")]
+[assembly: AssemblyTitle("DimWin Brightness")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("StinkyPat Game Studios")]
-[assembly: AssemblyProduct("dimwin Brightness")]
+[assembly: AssemblyCompany("reblGreen Software Ltd")]
+[assembly: AssemblyProduct("DimWin Brightness")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
